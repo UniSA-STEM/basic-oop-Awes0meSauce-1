fd978e4a0826eb3b4bd7d462fca3afcb90903ba9
Awes0meSauce <78591427+Awes0meSauce@users.noreply.github.com>
2025-10-09 03:05:21 +1030

    Created the class Hacker and created the __init_ function for the class.

--- Hacker.py
+++ Hacker.py
@@ -6,3 +6,10 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Hacker:
+    def __init__(self):
+        self.name = "Hacker"
+        self.inventory = ""
+        self.rig = False
+        self.trace_level = 0
