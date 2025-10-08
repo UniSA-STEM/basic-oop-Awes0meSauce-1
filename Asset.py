68b1ddcb407eaa6ce7ab5df15078c13bbd439f9d
Awes0meSauce <78591427+Awes0meSauce@users.noreply.github.com>
2025-10-09 05:55:37 +1030

    Added all the functions for Asset and Rig

--- Asset.py
+++ Asset.py
@@ -6,3 +6,16 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+class Asset:
+    def __init__(self):
+        self.name = "Asset"
+        self.description = ""
+        self.encrypted = False
+
+    def __str__(self):
+        if not self.encrypted:
+           return str(self.name)
+
+        elif self.encrypted:
+             return None
+        return None
--- Rig.py
+++ Rig.py
@@ -8,4 +8,20 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
 class Rig:
-    def __init__(self):
+    def __init__(self):        self.name = "Rig"
+        self.damage_counter = 0
+        self.broken = False
+        self.storage = ""
+        self.upgrade_level = 0
+
+    def upgrade_level(self):
+        self.upgrade_level += 1
+
+    def damage(self):
+        self.damage_counter += 1
+
+    def asset_generator(self):
+        self.storage = "Rig"
+
+    def __str__(self):
+        return str(self.name)
