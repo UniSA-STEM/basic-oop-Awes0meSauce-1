6a2cda54b9adb0783887263ed1474b790100b1e8
Awes0meSauce <78591427+Awes0meSauce@users.noreply.github.com>
2025-10-20 16:34:36 +1030

    Update the description of the classes

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 """
 File: main.py
 Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Author: Jack Gallagher
+ID: 110410979
+Username: galjh002
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
